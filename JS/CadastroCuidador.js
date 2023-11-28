dec479126454bc36b2b811f4a3b9f511a81b0833
AbilioGamaNJ <abiliogamanetojunior@gmail.com>
2023-11-28 13:38:47 -0300

    mudanças em rotas e retiro do banco de dados

--- JS/CadastroCuidador.js
+++ JS/CadastroCuidador.js
@@ -1,56 +1,56 @@
-var nome_cuidador = document.getElementById("nome_cuidador")
-var email_cuidador = document.getElementById("email_cuidador")
-var celular_cuidador = document.getElementById("celular_cuidador")
-var cpf_cuidador = document.getElementById("cpf_cuidador")
-var senha_cuidador = document.getElementById("senha_cuidador")
+// var nome_cuidador = document.getElementById("nome_cuidador")
+// var email_cuidador = document.getElementById("email_cuidador")
+// var celular_cuidador = document.getElementById("celular_cuidador")
+// var cpf_cuidador = document.getElementById("cpf_cuidador")
+// var senha_cuidador = document.getElementById("senha_cuidador")
 
 
-    function Cadastro(){
+//     function Cadastro(){
    
-        const [nome_cuidador, setNome_cuidador] = useState("");
-        const [email_cuidador, setEmail_cuidador] = useState("");
-        const [celular_cuidador, setCelular_cuidador] = useState("");
-        const [cpf_cuidador, setCpf_cuidador] = useState("");
-        const [senha_cuidador, setSenha_cuidador] = useState("");
+//         const [nome_cuidador, setNome_cuidador] = useState("");
+//         const [email_cuidador, setEmail_cuidador] = useState("");
+//         const [celular_cuidador, setCelular_cuidador] = useState("");
+//         const [cpf_cuidador, setCpf_cuidador] = useState("");
+//         const [senha_cuidador, setSenha_cuidador] = useState("");
       
-        //  const callAgentFinder = async() => {
-        //    try{
-        //         const response = await fetch('http://localhost:3001/api/cuidador')
-        //         const data = response.json()
-        //      data.then(
-        //           (val) => {setCuidador(val.data)
+//         //  const callAgentFinder = async() => {
+//         //    try{
+//         //         const response = await fetch('http://localhost:3001/api/cuidador')
+//         //         const data = response.json()
+//         //      data.then(
+//         //           (val) => {setCuidador(val.data)
       
-        //            }
-        //         )
-        //     }catch(error){
-        //     }
-        //  }
+//         //            }
+//         //         )
+//         //     }catch(error){
+//         //     }
+//         //  }
 
-         const postCuidador = async () => {
-            if (nome_cuidador !== "" && email_cuidador !== "" && celular_cuidador !== "" && cpf_cuidador !== "" && senha_cuidador !== "" ) {
-              try {
-                const requestOptions = {
-                  method: "POST",
-                  headers: { "Content-type": "application/json" },
-                  body: JSON.stringify({
-                    nome_cuidador: nome_cuidador,
-                    email_cuidador: email_cuidador,
-                    celular_cuidador: celular_cuidador,
-                    cpf_cuidador: cpf_cuidador,
-                    senha_cuidador: senha_cuidador
-                  }),
-                };
-                await fetch("http://localhost:3001/api/cuidador", requestOptions);
-                window.location.href = "./loginCuidador.html";
-              } catch (error) {
-                console.log(error);
+//          const postCuidador = async () => {
+//             if (nome_cuidador !== "" && email_cuidador !== "" && celular_cuidador !== "" && cpf_cuidador !== "" && senha_cuidador !== "" ) {
+//               try {
+//                 const requestOptions = {
+//                   method: "POST",
+//                   headers: { "Content-type": "application/json" },
+//                   body: JSON.stringify({
+//                     nome_cuidador: nome_cuidador,
+//                     email_cuidador: email_cuidador,
+//                     celular_cuidador: celular_cuidador,
+//                     cpf_cuidador: cpf_cuidador,
+//                     senha_cuidador: senha_cuidador
+//                   }),
+//                 };
+//                 await fetch("http://localhost:3001/api/cuidador", requestOptions);
+//                 window.location.href = "./loginCuidador.html";
+//               } catch (error) {
+//                 console.log(error);
               
-              }
-            } else {
-              // alert("vazio")
-            }}
-            return postCuidador;
-}
+//               }
+//             } else {
+//               // alert("vazio")
+//             }}
+//             return postCuidador;
+// }
 
 function ShowPassword(){
     var inputPass = document.getElementById('senha_cuidador')
--- JS/a.js
+++
@@ -1,6 +0,0 @@
-num = 2
-if(num < 1){
-num *= (-1)
-
- for (x in range(1, num))
- console.log(x)}
